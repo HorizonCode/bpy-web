cf91bd9d6195aa61ee49a6bb42e75977838c4741
HorizonCode <horizoncode88@gmail.com>
2024-02-09 14:55:46 +0100

    add avatar icon and home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
 
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
@@ -29,33 +29,10 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">bpy-web</strong>
 			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+      <svelte:fragment slot="trail">
+				<Avatar initials="U" class="w-10 select-none cursor-pointer hover:ring hover:ring-gray-600 transition-all" />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
-<div class="container h-full w-screen mx-auto flex justify-center items-center">
-	<div class="bg-gray-800 w-screen h-full">
+<div class="h-full w-screen">
+	<div class="bg-pink-800/40 w-screen h-[500px]">
+    <div class="h-full flex flex-col justify-center items-center text-center gap-5">
+      <div class="flex flex-col gap-2">
+        <h1 class="h1">bpy-web</h1>
+        <h4 class="h4">bpy-web is a frontend for bancho.py made with modern frameworks for the best user experience.</h4>
+      </div>
+      <div>
+        <button class="btn variant-filled-primary rounded-lg">
+          Sign Up
+        </button>
+        <button class="btn variant-filled-surface rounded-lg">
+          Login
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="w-screen">
 
   </div>
 </div>
