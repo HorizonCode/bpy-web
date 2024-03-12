480c7ac46bfffc7215e2f5498a0fcf1a15cc8207
HorizonCode <horizoncode88@gmail.com>
2024-03-12 16:10:40 +0100

    override error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const errorCode = $page.status;
+	const errorMessage = $page.error?.message;
+</script>
+
+<div class="container mx-auto w-full p-5">
+	<div class="mx-auto card p-6 py-12">
+		<div
+			class="w-full flex flex-col lg:divide-x divide-surface-500 lg:flex-row items-center justify-around gap-5 lg:gap-2"
+		>
+			<div class="flex flex-col items-center justify-center">
+				<p class="text-4xl">{errorCode}</p>
+				<p class="text-xl">{errorMessage}</p>
+				<a class="mx-auto mt-10 btn variant-filled-surface" href="/">Back to Home</a>
+			</div>
+		</div>
+	</div>
+</div>
