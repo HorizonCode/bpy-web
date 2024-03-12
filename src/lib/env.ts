126903089e071ad4e882d60d434351c67ac2d073
HorizonCode <horizoncode88@gmail.com>
2024-03-12 11:03:29 +0100

    add appUrl

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,6 @@
 import { env } from "$env/dynamic/public";
 
 export const appName = env.PUBLIC_APP_NAME;
+export const appUrl = env.PUBLIC_APP_URL;
 export const apiUrl = env.PUBLIC_API_URL;
 export const avatarUrl = env.PUBLIC_AVATAR_URL;
