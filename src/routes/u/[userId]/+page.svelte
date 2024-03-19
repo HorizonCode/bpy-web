98b9a2325011864f74c182f41ae993590c4d372c
HorizonCode <horizoncode88@gmail.com>
2024-03-19 19:47:27 +0100

    add blur for now to indicate loading

--- src/routes/u/[userId]/+page.svelte
+++ src/routes/u/[userId]/+page.svelte
@@ -581,7 +581,9 @@
 									>
 										Best Performance
 									</p>
-									<div class="flex flex-col gap-1">
+									<div
+										class="flex flex-col gap-1 transition-all{loading ? ' blur-sm' : ' blur-none'}"
+									>
 										<UserScores scores={bestScores} {currentMode} {currentType} />
 										<button
 											class="flex flex-row text-center justify-center items-center btn w-48 mx-auto variant-filled-surface px-4 py-1 mt-2 text-[0.7rem] leading-5"
@@ -607,7 +609,9 @@
 									>
 										Recent
 									</p>
-									<div class="flex flex-col gap-1">
+									<div
+										class="flex flex-col gap-1 transition-all{loading ? ' blur-sm' : ' blur-none'}"
+									>
 										<UserScores scores={recentScores} {currentMode} {currentType} />
 										<button
 											class="flex flex-row text-center justify-center items-center btn w-48 mx-auto variant-filled-surface px-4 py-1 mt-2 text-[0.7rem] leading-5"
