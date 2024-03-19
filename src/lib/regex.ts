0ceb8f743f8b7424e28605360b369ffd7f9f38a6
HorizonCode <horizoncode88@gmail.com>
2024-03-19 13:46:38 +0100

    just remove trailing zeroes if number ge 100

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -2,4 +2,4 @@ export const removeClanTag = (str: string) =>
   str.replace(/^\[[^\]]*?\]\s/g, "");
 
 export const removeTrailingZeroes = (number: number) =>
-  number.toFixed(2).replace(/\.?00$/gm, "");
+  number >= 100 ? number.toFixed(2).replace(/\.?00$/gm, "") : number.toFixed(2);
