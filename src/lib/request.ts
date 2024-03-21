eb6b5ab1c24818885a24b8b1e491da15d01a03d0
HorizonCode <rapeon3879@gmail.com>
2024-03-21 12:06:09 +0000

    function to get clients ip for turnstile captcha

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,64 +1,12 @@
-import { apiUrl } from "./env";
-import type { Clan, MapInfo, MapScores, PlayerScores } from "./types";
+const ipHeaders = [
+  "cf-connecting-ip",
+  "x-forwarded-for",
+  "x-real-ip",
+]
 
-export const getClan = async (clanId: number) => {
-  try {
-    const requestedClanData = await fetch(
-      `${apiUrl}/get_clan?id=${clanId}`,
-    );
-    if (!requestedClanData.ok) return undefined;
-    return await requestedClanData.json() as Clan;
-  } catch {
-    return undefined;
-  }
-};
+export const getClientIP = (request: Request, fallback: string): string => {
+  for (const header of ipHeaders)
+    if (request.headers.has(header) && request.headers.get(header) != null) return request.headers.get(header) as string;
 
-export const getBeatmap = async (beatmapId: number) => {
-  try {
-    const requestedMapData = await fetch(
-      `${apiUrl}/get_map_info?id=${beatmapId}`,
-    );
-    if (!requestedMapData.ok) return undefined;
-    return await requestedMapData.json() as MapInfo;
-  } catch {
-    return undefined;
-  }
-};
-
-export const getBeatmapScores = async (
-  beatmapMd5: string,
-  mode: number,
-  scope: "best" | "recent",
-) => {
-  try {
-    const requestedMapData = await fetch(
-      `${apiUrl}/get_map_scores?md5=${beatmapMd5}&mode=${mode}&limit=50&scope=${scope}`,
-    );
-    if (!requestedMapData.ok) return undefined;
-    return await requestedMapData.json() as MapScores;
-  } catch {
-    return undefined;
-  }
-};
-
-export const getPlayerScores = async (opts: {
-  userId: number;
-  mode: number;
-  limit: number;
-  offset: number;
-  includeLoved?: boolean;
-  includeFailed?: boolean;
-  scope: "best" | "recent";
-}) => {
-  try {
-    const requestedMapData = await fetch(
-      `${apiUrl}/get_player_scores?id=${opts.userId}&mode=${opts.mode}&limit=${opts.limit}&offset=${opts.offset}&include_failed=${
-        opts.includeFailed ?? true
-      }&include_loved=${opts.includeLoved ?? false}&scope=${opts.scope}`,
-    );
-    if (!requestedMapData.ok) return undefined;
-    return await requestedMapData.json() as PlayerScores;
-  } catch {
-    return undefined;
-  }
-};
+  return fallback;
+}
