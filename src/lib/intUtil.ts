4191ca0be7c19869e2d515efe5d9de93c52655ba
HorizonCode <horizoncode88@gmail.com>
2024-04-03 15:48:36 +0200

    add more default cover images, based on users id

---
+++ src/lib/intUtil.ts
@@ -0,0 +1,10 @@
+export const hashCode = (num: number) => {
+  let hash = 0;
+  if (num === 0) return hash;
+  for (let i = 0; i < num.toString().length; i++) {
+    const char = num.toString().charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
